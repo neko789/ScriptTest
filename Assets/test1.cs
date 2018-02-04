1a3bae24ff72f1aa3e23c7db94f7a32c582e5592
takeru morimoto <nice.hermori@gmail.com>
2018-02-04 23:32:52 +0900

    scripttest課題提出
    通常課題と応用課題

---
+++ Assets/test1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test1: MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+            int[] array = { 1, 3, 5, 8, 13 };
+            for (int i = 0; i < array.Length; i++)
+            {
+                Debug.Log(array[i]);
+            }
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                Debug.Log(array[i]);
+            }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/test2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Boss
+    {
+        private int mp = 53;
+        public void Magic()
+        {
+        if (mp >= 5)
+        {
+            this.mp -= 5;
+            Debug.Log("魔法攻撃をした。残りMPは" + mp);
+        }
+        else
+        {
+            Debug.Log("MPが足りないため魔法が使えない");
+        }
+        }
+    }
+
+public class test2 : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+    Boss zako = new Boss();
+		for (int i = 0; i <=12; i++)
+    {
+        zako.Magic();
+    } 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
